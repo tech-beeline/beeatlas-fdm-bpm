61c83863ccd7acea55be19d8d3d5d565371ec587
Зайнагтдинова Галия Нафисовна <GZaynagtdinova@beeline.ru>
2024-04-02 09:19:45 +0300

    SFDM-1391 : init bpm service with Camunda

---
+++ src/main/java/ru/beeline/fdmbpm/FdmBpmApplication.java
@@ -0,0 +1,15 @@
+package ru.beeline.fdmbpm;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableProcessApplication
+@SpringBootApplication
+public class FdmBpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FdmBpmApplication.class, args);
+    }
+
+}
