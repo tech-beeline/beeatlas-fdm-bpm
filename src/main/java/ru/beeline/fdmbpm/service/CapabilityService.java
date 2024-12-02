716047394687e6ebe97ceb0ac6e342fa4d11b5a6
salikhanov <SAlikhanov@oms.beeline.ru>
2024-12-02 15:43:04 +0600

    SFDM-2124 Баг, не создается пакет

--- src/main/java/ru/beeline/fdmbpm/service/CapabilityService.java
+++ src/main/java/ru/beeline/fdmbpm/service/CapabilityService.java
@@ -58,9 +58,10 @@ public class CapabilityService {
             log.info("Receive products:" + products);
             if (products.size() > 0) {
                 LOGGER.info("Register package, products size: {}", products.size());
-                PackageRegistrationResponseDTO responseDTO = packageClient.registerPackage("UPDATE_PRODUCTS", products.size());
-                ObjectNode messagePayload = objectMapper.createObjectNode();
+                PackageRegistrationResponseDTO responseDTO = packageClient.registerPackage("UPDATE_PRODUCTS"
+                        , products.size());
                 LOGGER.info("packageId: {}", responseDTO.getPackageId());
+                ObjectNode messagePayload = objectMapper.createObjectNode();
                 messagePayload.put("packageId", responseDTO.getPackageId());
                 messagePayload.put("payload", products.toString());
                 LOGGER.info("Send to package-queue");
