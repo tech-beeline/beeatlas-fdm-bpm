234f69ea351e1f6a113485521a50c76738b6f73d
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2024-12-11 13:01:55 +0300

    SFDM-1963 Процесс обработки данных с git и формирования связи приложения с технологией

--- src/main/java/ru/beeline/fdmbpm/service/RelationsService.java
+++ src/main/java/ru/beeline/fdmbpm/service/RelationsService.java
@@ -53,18 +53,18 @@ public class RelationsService {
 
     public void createRelations() {
         List<FdmGitlabLanguages> fdmGitlabLanguages = ringRepository.findUniqueCmdbCodeAndProjLangModify();
-        List<ProductDTO> productDTOS = techradarClient.getTech();
-        List<AliasLabelDTO> aliasLabelDTOS = productDTOS.stream()
-                .flatMap(productDTO -> productDTO.getTech().stream()
-                        .map(productTechDTO -> new AliasLabelDTO(
-                                productDTO.getProductId(),
-                                productDTO.getAlias(),
-                                productTechDTO.getId(),
-                                productTechDTO.getLabel())))
-                .collect(Collectors.toList());
-        removeMatchingObjects(aliasLabelDTOS, fdmGitlabLanguages);
-        aliasLabelDTOS.forEach(aliasLabelDTO ->
-                productClient.deleteRelation(aliasLabelDTO.getTechId(), aliasLabelDTO.getProductId()));
+//        List<ProductDTO> productDTOS = techradarClient.getTech();
+//        List<AliasLabelDTO> aliasLabelDTOS = productDTOS.stream()
+//                .flatMap(productDTO -> productDTO.getTech().stream()
+//                        .map(productTechDTO -> new AliasLabelDTO(
+//                                productDTO.getProductId(),
+//                                productDTO.getAlias(),
+//                                productTechDTO.getId(),
+//                                productTechDTO.getLabel())))
+//                .collect(Collectors.toList());
+//        removeMatchingObjects(aliasLabelDTOS, fdmGitlabLanguages);
+//        aliasLabelDTOS.forEach(aliasLabelDTO ->
+//                productClient.deleteRelation(aliasLabelDTO.getTechId(), aliasLabelDTO.getProductId()));
         try {
             LOGGER.info("Register package, operation: {} , fdmGitlabLanguages size: {}", OPERATION, fdmGitlabLanguages.size());
             PackageRegistrationResponseDTO responseDTO = packageClient.registerPackage(
