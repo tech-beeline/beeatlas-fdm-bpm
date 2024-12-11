b075e39984ac2af7c3211493ed826cd1657f7ded
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2024-12-11 10:48:17 +0300

    SFDM-1963 Процесс обработки данных с git и формирования связи приложения с технологией

--- src/main/java/ru/beeline/fdmbpm/gitrepository/RingRepository.java
+++ src/main/java/ru/beeline/fdmbpm/gitrepository/RingRepository.java
@@ -13,7 +13,7 @@ public interface RingRepository extends JpaRepository<FdmGitlabLanguages, Intege
 
     @Query(value = "SELECT DISTINCT ON (cmdb_code, proj_lang) " +
             "       cmdb_code, " +
-            "       proj_lang, " +
+            "       proj_lang " +
             "FROM v_fdm_gitlab_languages " +
             "ORDER BY cmdb_code, proj_lang, extraction_date DESC",
             nativeQuery = true)
