c13c407a4297f06b8791336d8af1be5a337e5d7d
salikhanov <SAlikhanov@oms.beeline.ru>
2025-02-18 15:18:49 +0600

    SFDM-2378 Разработать процесс экспорта сущностей ФДМ в excel

---
+++ src/main/java/ru/beeline/fdmbpm/service/ExportProcessService.java
@@ -0,0 +1,12 @@
+package ru.beeline.fdmbpm.service;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ExportProcessService {
+
+    public void processExportingToExcel(String EntityType, Integer DocId){}
+}
