6a6d687ad9e05390989a904a7926353bd6f535d8
salikhanov <SAlikhanov@oms.beeline.ru>
2025-03-19 11:51:20 +0600

    SFDM-2384 Разработать метод возвращающий текущий статус процесса

---
+++ src/main/java/ru/beeline/fdmbpm/config/FlywayConfig.java
@@ -0,0 +1,27 @@
+package ru.beeline.fdmbpm.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean
+    public Flyway flyway(@Qualifier("processesDataSource") DataSource dataSource) {
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .schemas("processes")
+                .baselineOnMigrate(true)
+                .load();
+    }
+
+    @Bean
+    public FlywayMigrationInitializer flywayInitializer(Flyway flyway) {
+        return new FlywayMigrationInitializer(flyway, null);
+    }
+}
