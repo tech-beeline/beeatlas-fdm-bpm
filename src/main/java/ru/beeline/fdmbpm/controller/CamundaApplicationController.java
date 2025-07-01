39ff4b25b336e9ac0b7267d81c0cb2a4ac2a9e42
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2025-07-01 16:53:24 +0300

    SFDM-2678 Сделать swagger на собственные методы сервиса (не от движка камунды)

--- src/main/java/ru/beeline/fdmbpm/controller/CamundaApplicationController.java
+++ src/main/java/ru/beeline/fdmbpm/controller/CamundaApplicationController.java
@@ -1,6 +1,11 @@
 package ru.beeline.fdmbpm.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +19,7 @@ import ru.beeline.fdmbpm.service.ApplicationService;
 import java.util.List;
 
 import static ru.beeline.fdmbpm.utils.Constants.USER_ID_HEADER;
+import static ru.beeline.fdmbpm.utils.Constants.USER_ROLES_HEADER;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -23,27 +29,32 @@ public class CamundaApplicationController {
     @Autowired
     ApplicationService applicationService;
 
+    @Operation(parameters = {@Parameter(in = ParameterIn.HEADER, name = USER_ROLES_HEADER, required = true, array = @ArraySchema(schema = @Schema(type = "string")))})
     @GetMapping("/application/nobody")
     public ResponseEntity<List<ApplicationDTO>> getAssignedApplications() {
         return ResponseEntity.status(HttpStatus.OK).body(applicationService.getAssignedApplications());
     }
 
+    @Operation(parameters = {@Parameter(in = ParameterIn.HEADER, name = USER_ID_HEADER, required = true, schema = @Schema(type = "string"))})
     @GetMapping("/application/author")
     public ResponseEntity<List<ApplicationDTO>> getApplicationsByAuthor(HttpServletRequest request) {
         Integer userId = Integer.valueOf(request.getHeader(USER_ID_HEADER));
         return ResponseEntity.status(HttpStatus.OK).body(applicationService.getApplicationsByAuthor(userId));
     }
 
+    @Operation(parameters = {@Parameter(in = ParameterIn.HEADER, name = USER_ID_HEADER, required = true, schema = @Schema(type = "string"))})
     @GetMapping("/application/executor")
     public ResponseEntity<List<ApplicationDTO>> getApplicationsByExecutor(HttpServletRequest request) {
         Integer userId = Integer.valueOf(request.getHeader(USER_ID_HEADER));
         return ResponseEntity.status(HttpStatus.OK).body(applicationService.getApplicationsByExecutor(userId));
     }
 
+    @Operation(parameters = {@Parameter(in = ParameterIn.HEADER, name = USER_ROLES_HEADER, required = true, array = @ArraySchema(schema = @Schema(type = "string")), description = "Роли пользователя"),
+            @Parameter(in = ParameterIn.HEADER, name = USER_ID_HEADER, required = true, schema = @Schema(type = "string"), description = "ID пользователя")})
     @PatchMapping("/application/{business_key}/executor")
     public ResponseEntity patchExecutorProcess(@PathVariable("business_key") String businessKey,
-                                                        @RequestParam(value = "next_status") String nextStatus,
-                                                        HttpServletRequest request) {
+                                               @RequestParam(value = "next_status") String nextStatus,
+                                               HttpServletRequest request) {
         return applicationService.patchExecutorProcess(businessKey, nextStatus, request);
     }
 
@@ -52,11 +63,12 @@ public class CamundaApplicationController {
         return applicationService.getApplicationsByBusinessKey(businessKey);
     }
 
+    @Operation(parameters = {@Parameter(in = ParameterIn.HEADER, name = USER_ID_HEADER, required = true, schema = @Schema(type = "string"))})
     @PatchMapping("/application/{business_key}/change-status/{status_alias}")
     public ResponseEntity patchChangeStatus(@PathVariable("business_key") String businessKey,
                                             @PathVariable("status_alias") String statusAlias,
                                             @RequestBody(required = false) CommentDTO commentDTO,
                                             HttpServletRequest request) {
-        return  applicationService.patchChangeStatus(businessKey, statusAlias, request, commentDTO);
+        return applicationService.patchChangeStatus(businessKey, statusAlias, request, commentDTO);
     }
 }
