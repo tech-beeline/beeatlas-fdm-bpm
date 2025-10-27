fda518c142682f131e73d60a130e77677873db2c
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2025-10-27 14:56:15 +0300

    SFDM-2890 Обогощать данные о продуктах информацией о критичности и владельце

--- src/main/java/ru/beeline/fdmbpm/service/InfrastructureService.java
+++ src/main/java/ru/beeline/fdmbpm/service/InfrastructureService.java
@@ -106,7 +106,7 @@ public class InfrastructureService {
             ownerNode.put("login", peopleDto.getLoginMsad());
             item.set("owner", ownerNode);
             log.info("Send to  update-product-owner-and-priority-by-cmdb");
-            rabbitService.sendMessage(" update-product-owner-and-priority-by-cmdb", objectMapper.writeValueAsString(item));
+            rabbitService.sendMessage("update-product-owner-and-priority-by-cmdb", objectMapper.writeValueAsString(item));
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
